05e53ed6107ee10d9041692fa3fba17b59fe1729
Kell <aldar.vs01@gmail.com>
2019-07-31 19:10:42 +0300

    kell

---
+++ plan.php
@@ -0,0 +1,8 @@
+<?php
+    phpinfo();
+    echo `whereis plan.php`;
+    date_default_timezone_set('Asia/Irkutsk');
+    $file='data-log.txt';
+    $date = date('m/d/Y h:i:s a', time());
+    file_put_contents($file, $date . PHP_EOL, FILE_APPEND | LOCK_EX);
+?>
